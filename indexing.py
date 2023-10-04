3effb11c0fc0285e2964f723017e2797f175c108
Nilhenrik <48207585+Nilhenrik@users.noreply.github.com>
2023-10-04 20:24:05 +0200

    finpuss

--- indexing.py
+++ indexing.py
@@ -7,14 +7,14 @@ if not pt.started():
 
 print("Reading file")
 df = pd.read_csv('collection.tsv', sep='\t', names=['docno', 'text'],dtype=str, encoding='utf-8')
-print("dataframe read")
+print("Dataframe read")
 df.dropna(inplace=True)
 res_dict = df.to_dict()
 # Indexing using PyTerrier
 index_loc = "./terrier_index"
 indexer = pt.IterDictIndexer(index_loc, overwrite=True, verbose=True)
-print("indexer made")
+print("Indexer made")
 index = indexer.index(df.to_dict(orient="records"))
 #index = indexer.index(res_dict["text"], res_dict["docno"])
-print("indexer indexed")
+print("Indexer indexed")
 print("Done")
