bbab4639b02b5237d870c46c73805aff3fca401a
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2023-10-19 11:54:23 +0200

    Introducing the rest

--- indexing.py
+++ indexing.py
@@ -1,5 +1,6 @@
 import pyterrier as pt
 import pandas as pd
+import pyterrier_doc2query
 if not pt.started():
   pt.init() 
 
@@ -8,12 +9,16 @@ print("Reading file")
 df = pd.read_csv('./data/collection.tsv', sep='\t', names=['docno', 'text'],dtype=str, encoding='utf-8')
 print("Dataframe read")
 df.dropna(inplace=True)
-res_dict = df.to_dict()
-# Indexing using PyTerrier
+doc2query = pyterrier_doc2query.Doc2Query(append=True) # append generated queries to the original document text
+
+# Indexing setup
 index_loc = "./terrier_index"
-indexer = pt.IterDictIndexer(index_loc, overwrite=True, verbose=True,tokeniser="UTFTokeniser")
+
+# We chain the doc2query transformation with the IterDictIndexer for indexing
+indexer = doc2query >> pt.IterDictIndexer(index_loc, overwrite=True, verbose=True, tokeniser="UTFTokeniser")
 print("Indexer made")
+
+# Trigger the indexing process
 index = indexer.index(df.to_dict(orient="records"))
-#index = indexer.index(res_dict["text"], res_dict["docno"])
 print("Indexer indexed")
 print("Done")
